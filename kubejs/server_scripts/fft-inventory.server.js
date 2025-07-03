924fbacda84c3890598137ba90644ac5b1d617da
Facundo Fierro <fierrof_47@hotmail.com>
2025-07-04 00:22:13 +0200

    update backpack reference to 'usefulbackpacks:backpack_small' and rename CURIO_SLOT to SLOT

--- kubejs/server_scripts/fft-inventory.server.js
+++ kubejs/server_scripts/fft-inventory.server.js
@@ -1,12 +1,12 @@
-const BACKPACK = 'backpacks:backpack';
-const CURIO_SLOT = 'back';
+const BACKPACK = 'usefulbackpacks:backpack_small';
+const SLOT = 'back';
 const INTERVAL = 5; // in ticks
 
 ServerEvents.tick(event => {
     if (event.server.tickCount % INTERVAL === 0) {
-        let withoutBackpackCommand = `execute if entity @p[curios=!{item:{id:"${BACKPACK}"},slot:["${CURIO_SLOT}"]}] run inventory_slots set_unlocked @p 0`;
+        let withoutBackpackCommand = `execute if entity @p[curios=!{item:{id:"${BACKPACK}"},slot:["${SLOT}"]}] run inventory_slots set_unlocked @p 0`;
         event.server.runCommandSilent(withoutBackpackCommand)
-        let withBackpackCommand = `execute if entity @p[curios={item:{id:"${BACKPACK}"},slot:["${CURIO_SLOT}"]}] run inventory_slots set_unlocked @p 27`;
+        let withBackpackCommand = `execute if entity @p[curios={item:{id:"${BACKPACK}"},slot:["${SLOT}"]}] run inventory_slots set_unlocked @p 27`;
         event.server.runCommandSilent(withBackpackCommand)
     }
 })
