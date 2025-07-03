80caf7d3c2061476503a4fec7baad766007b4082
Facundo Fierro <fierrof_47@hotmail.com>
2025-07-03 19:23:34 +0200

    refactor backpack handling commands for improved readability

--- kubejs/server_scripts/fft-inventory.server.js
+++ kubejs/server_scripts/fft-inventory.server.js
@@ -1,8 +1,12 @@
+const BACKPACK = 'backpacks:backpack';
+const CURIO_SLOT = 'back';
+const INTERVAL = 5; // in ticks
+
 ServerEvents.tick(event => {
-    if (event.server.tickCount % 5 === 0) {
-        let withoutBackpackCommand = `execute if entity @p[curios=!{item:{id:"backpacks:backpack"},slot:["back"]}] run inventory_slots set_unlocked @p 0`;
+    if (event.server.tickCount % INTERVAL === 0) {
+        let withoutBackpackCommand = `execute if entity @p[curios=!{item:{id:"${BACKPACK}"},slot:["${CURIO_SLOT}"]}] run inventory_slots set_unlocked @p 0`;
         event.server.runCommandSilent(withoutBackpackCommand)
-        let withBackpackCommand = `execute if entity @p[curios={item:{id:"backpacks:backpack"},slot:["back"]}] run inventory_slots set_unlocked @p 27`;
+        let withBackpackCommand = `execute if entity @p[curios={item:{id:"${BACKPACK}"},slot:["${CURIO_SLOT}"]}] run inventory_slots set_unlocked @p 27`;
         event.server.runCommandSilent(withBackpackCommand)
     }
 })
